632ae68438cf0f5b7f8e45be4b915e047b95698d
Ketlin Ciquelero <108404095+ketlinciquelero@users.noreply.github.com>
2023-03-18 22:39:58 -0300

    Questão 5

--- CincoPython.py
+++ CincoPython.py
@@ -1,5 +1,5 @@
 # Crie uma matriz bidimensional. Deverá ser solicitado três nomes de alunos e quatro notas, após solicitação dos nomes e das
-# notas deverá ser impresso os nomes acompanhados da média geral de cada aluno, deverá ser impresso também o nome do aluno que
+# notas deverá ser impresso os nomes acompanhados da média geral de cada aluno, também deverá ser impresso o nome do aluno que
 # obteve a maior média e o nome do aluno que obteve a menor média.
 
 matriz = [[0] * 4 for i in range(3)]
