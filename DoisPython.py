a7de1dd5a1cb7e985d6efcb439e74932498832d1
Ketlin Ciquelero <108404095+ketlinciquelero@users.noreply.github.com>
2023-03-20 04:20:53 -0300

    Questão 2
    Código atualizado e agora com comentários!
    
    Este código é um programa simples que pede ao usuário para digitar 5 números, armazena-os em uma lista chamada "numeros", e depois exibe o maior e o menor número dessa lista.
    
    - A primeira linha do código cria uma lista vazia chamada "numeros", que será preenchida posteriormente com os valores fornecidos pelo usuário.
    - O laço "for" é executado 5 vezes (já que a função "range(5)" gera uma sequência de números de 0 a 4). Dentro do laço, o programa solicita ao usuário que digite um número por meio da função "input()", armazena o valor digitado na variável "entrada", e adiciona esse valor à lista "numeros" usando o método "append()".
    - Após o laço ser concluído, o programa exibe o maior e o menor número da lista "numeros" usando as funções "max()" e "min()", respectivamente. Os valores máximo e mínimo são exibidos usando a função "print()", que exibe uma string formatada usando f-strings (ou formatação de string literal) do Python.

--- DoisPython.py
+++ DoisPython.py
@@ -1,9 +1,12 @@
-# Solicitar 5 números, ao fim, imprimir o número maior e o número menor.
-
 numeros = []
 
+# Laço de repetição para obter 5 entradas do usuário
 for i in range(5):
   entrada = input('Digite um número: ')
-  numeros.append(entrada)
+  numeros.append(float(entrada)) # adicionando cada entrada à lista de números
 
+# Exibindo o maior e o menor número da lista de números
 print(f'O maior número é {max(numeros)} e o menor número é {min(numeros)}')
+
+# Melhorias no código:
+# Foi adicionado a conversão de cada entrada para o tipo float para permitir a comparação de números decimais
