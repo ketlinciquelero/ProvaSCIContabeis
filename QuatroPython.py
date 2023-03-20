93c2d9546e43c5900760913c2d2b3b8b8fb0a2b5
Ketlin Ciquelero <108404095+ketlinciquelero@users.noreply.github.com>
2023-03-20 04:37:13 -0300

    Questão 4
    Código atualizado para criar uma lista com cinco posições.
    
    Este código solicita ao usuário que insira 5 números e armazena esses números em uma lista chamada "numeros". Em seguida, ele imprime o número armazenado na terceira posição (índice 2) da lista.

--- QuatroPython.py
+++ QuatroPython.py
@@ -1,9 +1,12 @@
-# Criar um vetor de cinco posições, solicitar  cinco números e ao fim, imprimir o valor apresentado na posição três.
+# Criar um vetor de cinco posições, solicitar cinco números e ao fim, imprimir o valor apresentado na posição três.
 
-numeros = []
+# Inicializa a lista "numeros" com 5 elementos vazios
+numeros = [None] * 5
 
+# Loop para solicitar que o usuário insira 5 números
 for i in range(5):
   numero = input(f'Digite o {i + 1}º número: ')
-  numeros.append(numero)
+  numeros[i] = numero
 
+# Imprime o número armazenado na terceira posição da lista
 print(f'O número na posição 3 é o: {numeros[2]}')
