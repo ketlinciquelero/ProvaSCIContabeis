7bf060cf299ead965f25222720260423744c50eb
Ketlin Ciquelero <108404095+ketlinciquelero@users.noreply.github.com>
2023-03-20 04:04:43 -0300

    Questão 1
    Atualizado para adicionar comentários e algumas alterações para deixar o código mais simples!
    
    Este código pede para o usuário digitar 5 números, e em seguida cria duas listas, uma para armazenar os números pares e outra para os ímpares. Em seguida, ele percorre a lista original de números e adiciona os números pares à lista de pares e os ímpares à lista de ímpares. Finalmente, ele calcula a média aritmética dos números da lista original e imprime os resultados.

--- UmPython.py
+++ UmPython.py
@@ -1,20 +1,26 @@
 # Solicitar a inserção de 5 números, ao final, imprimir os números pares, números ímpares e a média geral desses números.
 
+# Cria uma lista vazia para armazenar os números
 numeros = []
-pares = []
-impares = []
 
+# Pede para o usuário digitar 5 números e adiciona-os à lista
 for i in range(5):
   numero = int(input('Digite um número: '))
   numeros.append(numero)
 
+# Cria duas listas vazias, uma para os números pares e outra para os ímpares
+pares = []
+impares = []
+
+# Percorre a lista de números e adiciona os pares à lista de pares e os ímpares à lista de ímpares
 for j in numeros:
     if j % 2 == 0:
         pares.append(j)
+    else:
+        impares.append(j)
 
-for k in numeros:
-    if k % 2 != 0:
-        impares.append(k)
-
+# Calcula a média dos números da lista
 media = sum(numeros) / len(numeros)
+
+# Imprime os resultados, incluindo os números pares e ímpares e a média aritmética
 print(f'Os números pares são: {pares}\n Os números ímpares são: {impares}\n A média geral dos números é {round(media)}')
